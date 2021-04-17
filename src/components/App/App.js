38f2b032f1b4b5d16a11490fe8e3b886324dec1d
Cooper Saye <coopersaye@gmail.com>
2021-04-16 21:48:44 -0600

    Initialize firebase.

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,9 +1,31 @@
+import Header from '../Header/Header.js';
+import FileDisplay from '../FileDisplay/FileDisplay.js';
+import SignIn from '../SignIn/SignIn.js';
+
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
+import 'firebase/storage';
+import { firebaseConfig } from '../../util/firebaseConfig.js';
+import { useAuthState } from 'react-firebase-hooks/auth';
+
 import './App.css';
 
+firebase.initializeApp(firebaseConfig);
+
 function App() {
+  useAuthState(firebase.auth());
+
   return (
     <div className="App">
-    
+      {
+        firebase.auth().currentUser ?
+        <>
+          <Header />
+          <FileDisplay />
+        </> :
+        <SignIn />
+      }
     </div>
   );
 }
