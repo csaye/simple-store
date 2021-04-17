399e843cb351bf360bb89a1c2256f88cfd75b660
Cooper Saye <coopersaye@gmail.com>
2021-04-17 12:36:17 -0600

    Implement folder creation.

--- src/components/FileInput/FileInput.js
+++ src/components/FileInput/FileInput.js
@@ -6,19 +6,37 @@ import './FileInput.css';
 
 function FileInput(props) {
   const [file, setFile] = useState(undefined);
+  const [folderName, setFolderName] = useState('');
 
   async function uploadFile() {
     // if no file, return
-    console.log(file);
     if (!file) return;
     // put file in storage
     const uid = firebase.auth().currentUser.uid;
-    const storageRef = firebase.storage().ref(uid + '/' + file.name);
-    await storageRef.put(file).then(props.onUpdate);
+    const storageRef = firebase.storage().ref(uid + props.path + file.name);
+    await storageRef.put(file).then(props.onFileUpdate);
+  }
+
+  async function newFolder(e) {
+    e.preventDefault();
+    // return if no folder name
+    if (!folderName) return;
+    // add new folder in firebase
+    const uid = firebase.auth().currentUser.uid;
+    const foldersRef = firebase.firestore().collection('users').doc(uid).collection('folders');
+    await foldersRef.add({
+      name: folderName,
+      path: props.path
+    }).then(props.onFolderUpdate);
   }
 
   return (
     <div className="FileInput grid-box">
+      <p>{props.path}</p>
+      {
+        props.path !== '/' &&
+        <button onClick={props.leaveFolder}>Leave Folder</button>
+      }
       <input
         type="file"
         onChange={e => setFile(e.target.files[0])}
@@ -26,6 +44,14 @@ function FileInput(props) {
       {
         file && <button onClick={uploadFile}>Upload File</button>
       }
+      <form onSubmit={newFolder}>
+        <input
+          value={folderName}
+          onChange={e => setFolderName(e.target.value)}
+          required
+        />
+        <button type="submit">New Folder</button>
+      </form>
     </div>
   );
 }
