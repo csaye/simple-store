f08613285f2689c1c6d7001f158d4dc7faaf98cb
Cooper Saye <coopersaye@gmail.com>
2021-04-17 12:37:08 -0600

    Implement folder deletion.

--- src/components/Folder/Folder.js
+++ src/components/Folder/Folder.js
@@ -3,6 +3,41 @@ import firebase from 'firebase/app';
 import './Folder.css';
 
 function Folder(props) {
+  const { name, path, id } = props.folder;
+
+  // recursively deletes contents of folder
+  async function deleteFolderContents(path) {
+    const storageRef = firebase.storage().ref(path);
+    await storageRef.listAll().then(result => {
+      result.items.forEach(file => {
+        const fileRef = firebase.storage().ref(file.fullPath);
+        fileRef.delete();
+      });
+      result.prefixes.forEach(folder => {
+        deleteFolderContents(folder.fullPath);
+      });
+    })
+  }
+
+  async function deleteFolder() {
+    // delete folder document from firebase
+    const uid = firebase.auth().currentUser.uid;
+    const foldersRef = firebase.firestore().collection('users').doc(uid).collection('folders');
+    const folderRef = foldersRef.doc(id);
+    // delete folder in firebase
+    await folderRef.delete().then(props.onFolderUpdate);
+    // delete subfolders in firebase
+    await foldersRef.get().then(result => {
+      result.forEach(folder => {
+        if (folder.data().path.startsWith(path + name + '/')) {
+          folder.ref.delete();
+        }
+      });
+    });
+    // delete folder from storage
+    deleteFolderContents(uid + path + name);
+  }
+
   return (
     <div className="Folder grid-box">
       <h1>Folder</h1>
@@ -13,6 +48,7 @@ function Folder(props) {
       >
         Enter Folder
       </button>
+      <button onClick={deleteFolder}>Delete Folder</button>
     </div>
   );
 }
